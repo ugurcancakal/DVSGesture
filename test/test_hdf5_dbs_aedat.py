8c530342f4d6f235d217e269f3d55bedf5a84f70
Ugurcan Cakal <ugurcan.cakal@gmail.com>
2020-11-16 22:07:06 +0300

    extract event test completed

--- test/test_hdf5_dbs_aedat.py
+++ test/test_hdf5_dbs_aedat.py
@@ -7,6 +7,8 @@ import sys
 sys.path.insert(0,'..')
 
 import unittest
+import aedat_file as af
+import process_aedat as pa
 import hdf5_dbs_aedat as dbs
 import test_env as env
 import numpy as np
@@ -17,6 +19,22 @@ class TestHdf5DBSAedat(unittest.TestCase):
 		'''
 		Extract events from aedat file
 		'''
+		root_dir = '/home/ugurc/drive/current/ee543/project/DVSGesture/test/test_files'
+		test_config = 'extract_event_test.txt'
+		test_filelist = pa.get_filelist(root_dir,test_config)
+		pickle_list = [name.replace('.aedat', '.pickle') for name in test_filelist]
+		events, labels = dbs.extract_events(test_filelist)
+
+		_events = np.empty(0)
+
+		for filename in pickle_list:
+			test = af.load_aedat_pickle(filename)
+			event = np.asarray([np.asarray(seq,dtype=np.uint32) for seq in test.seq_clipped])
+			_events = np.concatenate((_events, event))
+			
+		for event1, event2 in zip(events, _events):
+			self.assertTrue((event1 == event2).all())
+
 		print('File extraction Test passed!\n')
 	def test2_set_group(self):
 		'''
