13cc465bfd359ab97ccbda2d7ed690a9242ea3a3
Ugurcan Cakal <ugurcan.cakal@gmail.com>
2020-11-17 22:09:11 +0300

    Enhanced event extraction tests and added set_group test

--- test/test_hdf5_dbs_aedat.py
+++ test/test_hdf5_dbs_aedat.py
@@ -13,6 +13,7 @@ import hdf5_dbs_aedat as dbs
 import test_env as env
 import numpy as np
 import os
+import h5py
 
 class TestHdf5DBSAedat(unittest.TestCase):
 	def test1_extract_events(self):
@@ -35,11 +36,74 @@ class TestHdf5DBSAedat(unittest.TestCase):
 		for event1, event2 in zip(events, _events):
 			self.assertTrue((event1 == event2).all())
 
-		print('File extraction Test passed!\n')
+		self.assertEqual(events.shape,(9,))
+
+		print('File extraction Test on artificial aedat files has passed!\n')
+
+	# def test_1_1_extract_events_test(self):
+	# 	root_dir = '/home/ugurc/drive/data/DvsGesture'
+	# 	test_config = 'trials_to_test.txt'
+	# 	test_filelist = pa.get_filelist(root_dir,test_config)
+	# 	events, labels = dbs.extract_events(test_filelist)
+	# 	print('File extraction Test on test set has passed!\n')
+
+	# 	self.assertEqual(events.shape,(288,))
+
+	# def test_1_2_extract_events_train(self):
+	# 	root_dir = '/home/ugurc/drive/data/DvsGesture'
+	# 	test_config = 'trials_to_train.txt'
+	# 	train_filelist = pa.get_filelist(root_dir,test_config)
+	# 	events, labels = dbs.extract_events(train_filelist)
+	# 	print('File extraction Test on training set has passed!\n')
+
+	# 	self.assertEqual(events.shape,(1176,))
+
+
 	def test2_set_group(self):
 		'''
 		Group creation test
 		'''
+		hdf5_dir = 'test_files/dvs_gestures_unittest.hdf5'
+		root_dir = '/home/ugurc/drive/current/ee543/project/DVSGesture/test/test_files'
+		test_config = 'extract_event_test.txt'
+		test_filelist = pa.get_filelist(root_dir,test_config)
+		events, labels = dbs.extract_events(test_filelist)
+		_events = []
+		_labels = []
+
+		for i,event in enumerate(events):
+			if not event.size == 0:
+				_events.append(event)
+				_labels.append(labels[i])
+
+		# Create a unit_test group
+		try:
+			with h5py.File(hdf5_dir, 'w') as hdf:
+				dbs.set_group(hdf, 'unit_test', test_filelist)
+		except:
+			print("Failed to create unit_test set")
+
+		# Check if all created correctly
+		try:
+			with h5py.File(hdf5_dir,'r') as hdf:
+				for group, event, lab in zip(hdf['unit_test'].keys(), _events, _labels):
+					time = hdf.get('unit_test/{}/time'.format(group))
+					pos = hdf.get('unit_test/{}/pos'.format(group))
+					label = hdf.get('unit_test/{}/label'.format(group))
+
+					print('\n\n\n',event)
+					print(lab,'\n\n\n')
+					print(np.array(time))
+					print(np.array(pos))
+					print(np.array(label))
+							# print(items)
+							# print(list(hdf[_index]))
+
+		except:
+			print("Failed to read the hdf5 file")
+
+
+
 		print('Group creation test has passed!\n')
 
 	def test3_create_dbs(self):
